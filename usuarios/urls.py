c69482a5cb07e6e2eb06a17b8eee7c32ada07f80
Oriol <oriolbanos@protonmail.com>
2024-11-18 18:40:01 +0100

    polla

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,20 +2,19 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
-from .views import listar_usuarios, crear_usuario
+from .views import listar_usuarios, crear_usuario, usuario_exitoso
 
 urlpatterns = [
     # Aquí irían otras URLs de tu proyecto
     # ejemplo de cómo incluir URLs de una app
     path('', listar_usuarios, name='listar_usuarios'),
     path('crear-usuario/', crear_usuario, name='crear_usuario'),
-
-    # Puedes agregar aquí más rutas que necesites para tu proyecto
+    path('usuario_exitoso/', usuario_exitoso, name='usuario_exitoso')
 ]
 
 # En desarrollo, servir archivos estáticos y de medios
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
-                        document_root=settings.STATIC_ROOT)
+                          document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL,
-                        document_root=settings.MEDIA_ROOT)
+                          document_root=settings.MEDIA_ROOT)
