d772ce1896fdb7bd8649412c9640cb083f43fe16
Ferran Bujalance Devesa <162443815+Tabuca10@users.noreply.github.com>
2024-11-18 16:33:47 +0100

    Error de bucle de URLs corregido

--- pawmeet/settings.py
+++ pawmeet/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home',  # primera pagina creada ,
+    'home',  # primera pagina creada
     'usuarios'
 ]
 
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,12 +2,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
-from . import views
+from .views import listar_usuarios, crear_usuario
 
 urlpatterns = [
     # Aquí irían otras URLs de tu proyecto
     # ejemplo de cómo incluir URLs de una app
-    path('usuarios/', include('usuarios.urls')),
+    path('', listar_usuarios, name='listar_usuarios'),
 
     # Puedes agregar aquí más rutas que necesites para tu proyecto
 ]
@@ -15,6 +15,6 @@ urlpatterns = [
 # En desarrollo, servir archivos estáticos y de medios
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
+                        document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+                        document_root=settings.MEDIA_ROOT)
