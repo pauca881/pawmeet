0a45e33546f91bbe1698d0880a442f852e95dec2
Ferran Bujalance Devesa <162443815+Tabuca10@users.noreply.github.com>
2024-11-29 16:07:00 +0100

    Retoques en la página de conectar

--- home/views.py
+++ home/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-
-
+# Our views
 def home_view(request):
     return render(request, 'home.html')
 
