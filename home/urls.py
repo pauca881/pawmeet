945855426179ec01e2ca75d93abfd82c3e20459f
Ferran Bujalance Devesa <162443815+Tabuca10@users.noreply.github.com>
2024-12-05 18:30:33 +0100

    Modificaciones htmlcss

--- home/urls.py
+++ home/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('mascotas_cercanas/', views.ver_mascota_cercana_view, name='mascotas_cercanas'),
     path('list_mascotas/', views.list_mascotas_view, name='list_mascotas'),
     path('meetups/', views.eventos_view, name='eventos'),
-
+    path('home/', views.home_view, name='home')
 ]
